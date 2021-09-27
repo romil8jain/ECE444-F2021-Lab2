61dab6d1cb74e1d6cf07ade8f05b9e9d83fe880a
romil8jain <57597689+romil8jain@users.noreply.github.com>
2021-09-26 23:35:46 -0400

    Added images and paritally completed activity 4

--- hello.py
+++ hello.py
@@ -1,16 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash, session
 from flask_bootstrap import Bootstrap
 from datetime import datetime
-now = datetime.now()
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[DataRequired(), validEmail])
+    submit = SubmitField('Submit')
+
+    
+
+def validEmail(form, email):
+        if email is not None and not email.find('@'):
+            raise ValidationError(f'Please include an @ in the email address, {email} is missing @')
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    now = datetime.now()
-    date_time = now.strftime("%A, %B %-d, %Y %H:%M PM")
-    return render_template('index.html', name = "Romil!", date_time=date_time)
+    # session['name'] = None
+    # session['email'] = None
+    form = NameForm()
+    if form.validate_on_submit():
+        if form.email.data is not None and not form.email.data.find('utoronto'):
+            flash('Please fill in a UofT email')
+        session['name'] = form.name.data
+        # form.name.data = ''
+        session['email'] = form.email.data
+        # form.email.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', name = session.get('name'), form = form, email = session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
